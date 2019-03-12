08172ba908b1aa9bdc78e4abebe3e8963301ff57
oh.kwon <slash512@gmail.com>
2019-03-12 21:33:36 +0900

    add to problem contents.

--- 4.Count8Numbers/Program.cs
+++ 4.Count8Numbers/Program.cs
@@ -4,6 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * 
+ * 1부터 10,000까지 8이라는 숫자가 총 몇번 나오는가?
+
+ * 8이 포함되어 있는 숫자의 갯수를 카운팅 하는 것이 아니라 8이라는 숫자를 모두 카운팅 해야 한다.
+ * (※ 예를들어 8808은 3, 8888은 4로 카운팅 해야 함)
+ * 
+ */
+
 namespace _4.Count8Numbers
 {
 	class Program
